bc849f95a6f5938aa6ded8370c9ebd9c7d56df04
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2017-12-18 23:49:48 +0100

    Feature: build slide mechanism

--- script/script.js
+++ script/script.js
@@ -1,19 +1,41 @@
-/*var button = document.getElementById('btn-left');
+const btnLeft = document.getElementById('btn-left');
+const btnRight = document.getElementById('btn-right');
+//console.log(button);
+const images = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg'
+];
 
-function buttonClick()
-{
-    alert('Przycisk działa!!!');
+function updateSrc(src) {
+    const image = document.getElementById('photo');
+    image.setAttribute('src', src);
 }
 
-button.nextElementSibling = buttonClick;*/
+let index = 0;
 
-
-
-
-    function zmiana()
-{
-    
-    var photo = document.getElementById("img1").src="img/2.jpg";
-    var photo = document.getElementById("img1").src="img/2.jpg";
+function leftButtonClick() {
+    index--;
+    if (index < 0) {
+        index = images.length - 1;
+    }
+    updateSrc(images[index]);
+    //console.log(index);
 }
-    
+
+
+function rightButtonClick() {
+    index++;
+    if (index >= images.length) {
+        index = 0;
+    }
+    updateSrc(images[index]);
+    //console.log(index);
+}
+
+
+btnLeft.addEventListener('click', leftButtonClick);
+btnRight.addEventListener('click', rightButtonClick);
