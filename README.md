95020dc504f58a227e36ce6fb04db04627084a25
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2017-12-18 11:13:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Zadanie pt. "SLIDER"
+Link do strony: https://cezarytworzewski.github.io/slider/
+
+Zadanie polega na realizacji komponentu za pomocą 3 języków
+programowania: HTML, CSS oraz JavaScript.
+
+
---
+++ script/script.js
@@ -0,0 +1,19 @@
+/*var button = document.getElementById('btn-left');
+
+function buttonClick()
+{
+    alert('Przycisk działa!!!');
+}
+
+button.nextElementSibling = buttonClick;*/
+
+
+
+
+    function zmiana()
+{
+    
+    var photo = document.getElementById("img1").src="img/2.jpg";
+    var photo = document.getElementById("img1").src="img/2.jpg";
+}
+    
