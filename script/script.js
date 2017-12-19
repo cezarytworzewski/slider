a71147daf6708a6959d4a02c669da12784426552
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2017-12-19 21:34:29 +0100

    Add method setInterval: automatic change photo

--- script/script.js
+++ script/script.js
@@ -26,7 +26,6 @@ function leftButtonClick() {
     //console.log(index);
 }
 
-
 function rightButtonClick() {
     index++;
     if (index >= images.length) {
@@ -36,6 +35,27 @@ function rightButtonClick() {
     //console.log(index);
 }
 
+//function setInterval(updateSrc(), 3000);
+
 
 btnLeft.addEventListener('click', leftButtonClick);
 btnRight.addEventListener('click', rightButtonClick);
+
+setInterval("leftButtonClick()", 3000);
+
+
+
+
+/*const keyLeft = document.getElementById('key-left');
+const keyRight = document.getElementById('key-right');
+
+function leftKeyPress() {
+    
+}
+
+function rightKeyPress() {
+    
+}
+
+keyLeft.addEventListener('key', leftKeyPress);
+keyRight.addEventListener('key', rightKeyPress);*/
